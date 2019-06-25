13328dbdc0f3cf4808c667bfbc8bbbcc6c863361
XieHao <359402147@qq.com>
2019-06-25 18:04:08 +0800

    topht-lx

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home" ref="xdx">
+		萨嘎叫大家安静的歌i快速打开的哈拉省的sssssssssssssssssss水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水
+		<!--<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+		</ul>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+		</ul>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+		</ul>-->
+		<button @click="topdf">pdf</button>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	export default {
+		methods: {
+			var xdx=this.$refs.xhx
+			topdf() {
+				html2canvas(, {
+					onrendered: function(canvas) {
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+						var contentWidth = canvas.width;
+						var contentHeight = canvas.height;
+
+						//一页pdf显示html页面生成的canvas高度;
+						var pageHeight = contentWidth / 595.28 * 841.89;
+						//未生成pdf的html页面高度
+						var leftHeight = contentHeight;
+						//pdf页面偏移
+						var position = 0;
+						//a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+						var imgWidth = 555.28;
+						var imgHeight = 555.28 / contentWidth * contentHeight;
+
+						var pageData = canvas.toDataURL('image/jpeg', 1.0);
+
+						var pdf = new jsPDF('', 'pt', 'a4');
+						//有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+						//当内容未超过pdf一页显示的范围，无需分页
+						if(leftHeight < pageHeight) {
+							pdf.addImage(pageData, 'JPEG', 20, 0, imgWidth, imgHeight);
+						} else {
+							while(leftHeight > 0) {
+								pdf.addImage(pageData, 'JPEG', 20, position, imgWidth, imgHeight)
+								leftHeight -= pageHeight;
+								position -= 841.89;
+								//避免添加空白页
+								if(leftHeight > 0) {
+									pdf.addPage();
+								}
+							}
+						}
+
+						pdf.save('content.pdf');
+					}
+				})
+			}
+		}
+	}
 </script>
+<style type="text/css" lang="scss">
+	.home {
+		width: 1000px;
+		height: 500px;
+		ul {
+			display: flex;
+			li {
+				flex: 1;
+				list-style: none;
+				border-bottom: 1px solid black;
+				border-right: 1px solid black;
+			}
+		}
+	}
+</style>
